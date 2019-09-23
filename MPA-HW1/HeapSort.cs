0cb3f423e661d7a34db6a66bb69fda3620d08c7c
Wings' might <wingsmight@gmail.com>
2019-09-23 16:09:36 +0700

    separate classes

---
+++ MPA-HW1/HeapSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPA_HW1
+{
+    class HeapSort
+    {
+        private int heapSize;
+        private Compare compareDelegate;
+
+        public void Sort(List<Human> arr, Compare compareDelegate)
+        {
+            this.compareDelegate = compareDelegate;
+            BuildHeap(arr);
+            for (int i = arr.Capacity - 1; i >= 0; i--)
+            {
+                Swap(arr, 0, i);
+                heapSize--;
+                Heapify(arr, 0);
+            }
+        }
+
+        private void BuildHeap(List<Human> arr)
+        {
+            heapSize = arr.Capacity - 1;
+            for (int i = heapSize / 2; i >= 0; i--)
+            {
+                Heapify(arr, i);
+            }
+        }
+        private void Swap(List<Human> arr, int x, int y)
+        {
+            Human temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        private void Heapify(List<Human> arr, int index)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+            if (left <= heapSize && compareDelegate.Invoke(arr[left], arr[index]) > 0)
+            {
+                largest = left;
+            }
+
+            if (right <= heapSize && compareDelegate.Invoke(arr[right], arr[largest]) > 0)
+            {
+                largest = right;
+            }
+
+            if (largest != index)
+            {
+                Swap(arr, index, largest);
+                Heapify(arr, largest);
+            }
+        }
+    }
+}
---
+++ MPA-HW1/Human.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPA_HW1
+{
+    class Human : IComparer<Human>
+    {
+        private string name;
+        private string secondName;
+        private int birthYear;
+
+        public Human(string name, string secondName, int birthYear)
+        {
+            this.name = name;
+            this.secondName = secondName;
+            this.birthYear = birthYear;
+        }
+
+        public int Compare(Human x, Human y)
+        {
+            return (x.BirthYear).CompareTo(y.birthYear);
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string SecondName
+        {
+            get { return secondName; }
+        }
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+    }
+}
